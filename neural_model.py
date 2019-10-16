daf574c89764ffb146e0dc4719bfeed301a7b835
Stephen Jonany <sjonany@gmail.com>
2019-10-16 11:35:30 -0700

    Fixed compute_vth. Validated against interactome's by adding print statements and pdbs. Please see compute_vth() for validation results.

--- neural_model.py
+++ neural_model.py
@@ -1,5 +1,6 @@
 import numpy as np
-from numpy import linalg
+from scipy import linalg, sparse
+import pdb
 
 class NeuralModel:
   """ The C elegans model as described in the paper: "Neural Interactome: Interactive Simulation of a Neuronal System"
@@ -28,7 +29,7 @@ class NeuralModel:
     self.seed = None
 
     # TODO: Set params = I_ext, an array of constants. Most are zeroes.
-    self.I_ext = np.reshape(np.zeros(self.N), (self.N,1))
+    self.I_ext = np.zeros(self.N)
 
     # Cell membrane capacitance. 1.5 pF / 100 = 0.015 arb (arbitrary unit)
     self.C = 0.015
@@ -76,13 +77,26 @@ class NeuralModel:
     self.compute_Vth()
 
   def compute_Vth(self):
+    """
+    Vth computation that I wrote from scratch, and that matches my math derivations more.
+    Validations:
+    - Ran interactome code and printed out their Vth.
+      Vth 1 30 100, sum = -18.3342063908 -6.2498993778 -3.61729185518 -1194.25458719
+    - compute_Vth_interactome(), which is a near copy paste, produces very similar results.
+      Vth 1 30 100, sum = -18.334206390780512 -6.249899377800607 -3.617291855178621 -1194.2545871854843
+    - compute_Vth(), this method, produces very similar results as well.
+      Vth 1 30 100, sum = -18.334206390780512 -6.249899377800608 -3.6172918551786215 -1194.2545871854845
+    - L2 norm of compute_Vth() and compute_Vth_interactome():  3.907985046680551e-14
+    """
+
     b1 = -np.tile(self.Gc * self.Ec, (self.N, 1))
-    s_eq = self.ar / (self.ar + 2 * self.ad)
-    b3 = -s_eq * (self.Gg @ self.E)
+    # Interactome rounded to 4, so we followed suit.
+    s_eq = round(self.ar / (self.ar + 2 * self.ad), 4)
+    b3 = -s_eq * (self.Gs @ self.E)
 
     m1 = -self.Gc * np.identity(self.N)
     # N x 1, where each item is a row sum
-    Gg_row_sums =  self.Gg.sum(axis = 1)
+    Gg_row_sums = self.Gg.sum(axis = 1)
     # m2 is a diagonal matrix with the negative row sums as the values
     m2 = - np.diag(Gg_row_sums)
     Gs_row_sums =  self.Gs.sum(axis = 1)
@@ -93,18 +107,61 @@ class NeuralModel:
     m4 = self.Gg
 
     A = m1 + m2 + m3 + m4
-    b = b1 + b3 - self.I_ext
+    # If you don't reshape I_ext to be an Nx1 vector, you will get buggy results.
+    b = b1 + b3 - np.reshape(self.I_ext, (self.N,1))
     self.Vth = linalg.solve(A, b)
-    # TODO: Reread code, then verify value against interactome w/ 0 injection.
+
+  def compute_Vth_interactome(self):
+    """
+    Vth computation using interactome's way. I copied pasted the code with very minimal modifications.
+    This method is not used, unless you want to exactly replicate interactome's results without rounding differences.
+    """
+    N = self.N
+    # Modified from
+    # Gcmat = np.multiply(Gc, np.eye(N)); M1 = -Gcmat
+    M1 = -self.Gc * np.identity(N)
+    # Modified from
+    # b1 = np.multiply(Gc, EcVec)
+    b1 = np.tile(self.Gc * self.Ec, (N, 1))
+
+    # Modified from
+    # Ggap = np.multiply(ggap, Gg)
+    # Our code already computes total conductance
+    Ggap = self.Gg
+    Ggapdiag = np.subtract(Ggap, np.diag(np.diag(Ggap)))
+    Ggapsum = Ggapdiag.sum(axis = 1)
+    Ggapsummat = sparse.spdiags(Ggapsum, 0, N, N).toarray()
+    M2 = -np.subtract(Ggapsummat, Ggapdiag)
+
+    # Modified from
+    # Gs_ij = np.multiply(gsyn, Gs)
+    Gs_ij = self.Gs
+    s_eq = round((self.ar/(self.ar + 2 * self.ad)), 4)
+    sjmat = np.multiply(s_eq, np.ones((N, N)))
+    S_eq = np.multiply(s_eq, np.ones((N, 1)))
+    Gsyn = np.multiply(sjmat, Gs_ij)
+    Gsyndiag = np.subtract(Gsyn, np.diag(np.diag(Gsyn)))
+    Gsynsum = Gsyndiag.sum(axis = 1)
+    M3 = -sparse.spdiags(Gsynsum, 0, N, N).toarray()
+
+    b3 = np.dot(Gs_ij, np.multiply(s_eq, self.E))
+
+    M = M1 + M2 + M3
+
+    # Removed the LU's, because pretty much the same result anyways.
+
+    # (P, LL, UU) = linalg.lu(M)
+    # self.Vth = linalg.solve_triangular(UU, linalg.solve_triangular(LL, b, lower = True, check_finite=False), check_finite=False)
+    bbb = -b1 - b3
+    bb = np.reshape(bbb, N)
+
+    # Modified from
+    # InputMask = np.multiply(Iext, InMask)
+    InputMask = self.I_ext
+    b = np.subtract(bb, InputMask)
+    self.Vth = linalg.solve(M, b)
 
 # TODO: Implement run()
-
-# TODO: Delete the testing code below
 model = NeuralModel()
 model.init()
-print("Vth 1 30 100, sum =  {} {} {} {}".format(model.Vth[1], model.Vth[30], model.Vth[100], sum(model.Vth)))
-
-# Interactome's output:
-# Vth 1 30 100, sum =  -18.3342063908 -6.2498993778 -3.61729185518 -1194.25458719
-# Out output:
-# Vth 1 30 100, sum =  [-18.33333333] [-2.75030651] [-3.57268053] [-1114.86322726]
+print(model.Vth.sum())
