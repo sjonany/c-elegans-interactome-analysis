26fe158105733c86701c1a2381f1922cb3ae2479
Stephen Jonany <sjonany@gmail.com>
2019-10-20 17:25:48 -0700

    Exported html file of notebooks so newcomers can understand what's going on. Removed vth_interactome from model.py, because we are done validating.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # c-elegans-interactome-analysis
 Analysis scripts for https://github.com/shlizee/C-elegans-Neural-Interactome.
+Please see milestone_oscillation.html to see how everything comes together.
--- neural_model.py
+++ neural_model.py
@@ -91,13 +91,10 @@ class NeuralModel:
     Validations:
     - Ran interactome code and printed out their Vth.
       Vth 1 30 100, sum = -18.3342063908 -6.2498993778 -3.61729185518 -1194.25458719
-    - compute_Vth_interactome(), which is a near copy paste, produces very similar results.
-      Vth 1 30 100, sum = -18.334206390780512 -6.249899377800607 -3.617291855178621 -1194.2545871854843
     - compute_Vth(), this method, produces very similar results as well.
       Vth 1 30 100, sum = -18.334206390780512 -6.249899377800608 -3.6172918551786215 -1194.2545871854845
-    - L2 norm of compute_Vth() and compute_Vth_interactome():  3.907985046680551e-14
+    - L2 norm of compute_Vth() and interactome's:  3.907985046680551e-14
     """
-
     b1 = -np.tile(self.Gc * self.Ec, self.N)
     # Interactome rounded to 4, so we followed suit.
     s_eq = round(self.ar / (self.ar + 2 * self.ad), 4)
@@ -118,56 +115,6 @@ class NeuralModel:
     A = m1 + m2 + m3 + m4
     b = b1 + b3 - self.I_ext
     self.Vth = np.reshape(linalg.solve(A, b), self.N)
-
-  def compute_Vth_interactome(self):
-    """
-    Vth computation using interactome's way. I copied pasted the code with very minimal modifications.
-    This method is not used, unless you want to exactly replicate interactome's results without rounding differences.
-    """
-    N = self.N
-    # Modified from
-    # Gcmat = np.multiply(Gc, np.eye(N)); M1 = -Gcmat
-    M1 = -self.Gc * np.identity(N)
-    # Modified from
-    # b1 = np.multiply(Gc, EcVec)
-    b1 = np.tile(self.Gc * self.Ec, N)
-
-    # Modified from
-    # Ggap = np.multiply(ggap, Gg)
-    # Our code already computes total conductance
-    Ggap = self.Gg
-    Ggapdiag = np.subtract(Ggap, np.diag(np.diag(Ggap)))
-    Ggapsum = Ggapdiag.sum(axis = 1)
-    Ggapsummat = sparse.spdiags(Ggapsum, 0, N, N).toarray()
-    M2 = -np.subtract(Ggapsummat, Ggapdiag)
-
-    # Modified from
-    # Gs_ij = np.multiply(gsyn, Gs)
-    Gs_ij = self.Gs
-    s_eq = round((self.ar/(self.ar + 2 * self.ad)), 4)
-    sjmat = np.multiply(s_eq, np.ones((N, N)))
-    S_eq = np.multiply(s_eq, np.ones((N, 1)))
-    Gsyn = np.multiply(sjmat, Gs_ij)
-    Gsyndiag = np.subtract(Gsyn, np.diag(np.diag(Gsyn)))
-    Gsynsum = Gsyndiag.sum(axis = 1)
-    M3 = -sparse.spdiags(Gsynsum, 0, N, N).toarray()
-
-    b3 = np.dot(Gs_ij, np.multiply(s_eq, self.E))
-
-    M = M1 + M2 + M3
-
-    # Removed the LU's, because pretty much the same result anyways.
-
-    # (P, LL, UU) = linalg.lu(M)
-    # self.Vth = linalg.solve_triangular(UU, linalg.solve_triangular(LL, b, lower = True, check_finite=False), check_finite=False)
-    bbb = -b1 - b3
-    bb = np.reshape(bbb, N)
-
-    # Modified from
-    # InputMask = np.multiply(Iext, InMask)
-    InputMask = self.I_ext
-    b = np.subtract(bb, InputMask)
-    self.Vth = np.reshape(linalg.solve(M, b), self.N)
   
   def dynamic(self, t, state_vars):
     """Dictates the dynamics of the system.
