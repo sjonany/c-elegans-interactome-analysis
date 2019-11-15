c617336e4ce42bb25ce533c930f5d47ee08481b0
briardoty <briardoty@gmail.com>
2019-11-15 13:06:16 -0800

    Add util function to compute primary oscillatory period from fft, use in notebook

--- util/analysis_util.py
+++ util/analysis_util.py
@@ -1,22 +1,32 @@
 from scipy.fftpack import fft
 import scipy.signal as signal
+import numpy as np
+
+def get_fft(y, t):
+    N = len(y)
+    fft_y = fft(y)
+    fft_freq = np.linspace(0., 1./(2. * t), N//2)
+
+    fft_rs = np.reshape(fft_y, (N))
+    fft_rs = 2.0/N * np.abs(fft_rs[0:N//2])
+
+    return (fft_rs, fft_freq)
 
 def get_dominant_periods(projected, t):
-    N = len(projected)
-    pc_fft = fft(projected[:,0])
-    pc_fft_freq = np.linspace(0., 1./(2. * t), N//2)
-
+    (fft_rs, fft_freq) = get_fft(projected[:,0], t)
     widths = np.arange(1,10)
-    pc_fft_rs = np.reshape(pc_fft, (N))
-    pc_fft_rs = 2.0/N * np.abs(pc_fft_rs[0:N//2])
-    peaks = signal.find_peaks_cwt(pc_fft_rs, widths, min_length=1)
+    peaks = signal.find_peaks_cwt(fft_rs, widths, min_length=1)
+    freqs = fft_freq[peaks]
 
-    # TODO: invert the frequency to return periods instead of peak indices?
-    return peaks
+    return 1./freqs
 
 def get_dominant_period(projected, t):
-    # TODO: get single dominant period just using max(fft)
-    return
+    (pc_fft, pc_fft_freq) = get_fft(projected[:,0], t)
+    peak = np.where(pc_fft == np.amax(pc_fft))
+    freq_1 = pc_fft_freq[peak]
+    per_1 = 1./freq_1[0]
+
+    return per_1
 
 
 
